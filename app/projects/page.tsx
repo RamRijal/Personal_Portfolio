01e1e093542444f5be3abbd2683290fefb26b767
Ram Rijal <87277221+RamRijal@users.noreply.github.com>
2024-12-11 14:14:19 +0545

    Responsiveness Update and Nav Update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,86 +2,73 @@
 import { ProjectsList } from "@/data/data";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 
 const Projects = () => {
-
-  // const [projectName, setProjectName] = useState()
-  // const [description, setDescription] = useState()
   return (
-    <div className=" relative h-auto bg-violet-950 bg-none text-white overflow-hidden">
-      <div className="">
-        <h1 className="projects relative pt-9 text-center text-md font-normal text-gray-300">Check out my works</h1>
-        <div className="relative top-0 inline-flex xl:ml-[600px]">
+    <div className="relative h-auto bg-violet-950 text-white overflow-hidden">
+      <div className="text-center pt-9">
+        <h1 className="projects text-md font-normal text-gray-300">Check out my works</h1>
+        <div className="relative inline-flex ">
           <span className="absolute inset-x-0 bottom-0 border-b-[16px] border-[#0B8DB2]"></span>
-          <h1 className="projects relative pt-2 text-center text-5xl font-semibold">PROJECTS</h1>
+          <h1 className="projects relative pt-2 text-5xl font-semibold">PROJECTS</h1>
         </div>
       </div>
-      <div className="p-32 pt-12 pb-0">
-        <article className="grid grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-          {ProjectsList.map((project) => {
-            return (
-              <Link
-                key={project.id}
-                style={{ backgroundColor: project.color }}
-                className={`w-full flex flex-col  rounded-2xl shadow-lg overflow-hidden group relative items-start justify-center p-8 lg:p-4 lg:aspect-video cursor-pointer`}
-                href={project.redirectLink}
-                target="_blank">
-                <Image
-                  alt={project.title}
-                  loading="lazy"
-                  width="1366"
-                  height="677"
-                  decoding="async"
-                  data-nimg="1"
-                  className="absolute scale-[200%] blur-sm lg:blur-none group-hover:scale-[205%] lg:translate-x-1/2 rounded-tl-2xl shadow-lg shadow-darkest/20 opacity-30 group-hover:opacity-50 lg:scale-100 lg:group-hover:scale-[103%] transition-all duration-300 ease-in-out z-0 lg:-bottom-1"
-                  src={project.imgSrc}
-                />
-                <div className="flex flex-col w-1/2">
-                  <h3 className="text-2xl text-white font-display font-extrabold z-10">
-                    {project.title}
-                  </h3>
-                  <p className="text-white z-10 font-semibold">
-                    {project.description.slice(0, 30)}                  </p>
-                  <div className="project-link-icon-wrapper group-hover:text-[#AC58F5] absolute right-4 bottom-4 group-hover:rotate-[360]">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="w-5 h-5 icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-right">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                      <path d="M7 7l10 10"></path>
-                      <path d="M17 8l0 9l-9 0"></path>
-                    </svg>
-                  </div>
+
+      <div className="p-8 md:p-16 lg:px-32 lg:pt-20 pt-10 pb-0">
+        <article className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 max-w-7xl mx-auto">
+          {ProjectsList.map((project) => (
+            <Link
+              key={project.id}
+              href={project.redirectLink}
+              target="_blank"
+              style={{ backgroundColor: project.color }}
+              className="w-full flex flex-col rounded-2xl shadow-lg overflow-hidden group relative items-start justify-center p-8 lg:p-4 lg:aspect-video cursor-pointer"
+            >
+              <Image
+                alt={project.title}
+                loading="lazy"
+                width={1366}
+                height={677}
+                decoding="async"
+                data-nimg="1"
+                className="absolute scale-[200%] blur-sm lg:blur-none group-hover:scale-[205%] lg:translate-x-1/2 rounded-tl-2xl shadow-lg opacity-30 group-hover:opacity-50 lg:scale-100 lg:group-hover:scale-[103%] transition-all duration-300 ease-in-out z-0 lg:-bottom-1"
+                src={project.imgSrc}
+              />
+              <div className="flex flex-col w-1/2 z-10">
+                <h3 className="text-2xl font-extrabold">{project.title}</h3>
+                <p className="font-semibold">{project.description.slice(0, 30)}</p>
+                <div className="project-link-icon-wrapper absolute right-4 bottom-4 group-hover:text-[#AC58F5] group-hover:rotate-[360deg]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-5 h-5"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 7l10 10" />
+                    <path d="M17 8l0 9l-9 0" />
+                  </svg>
                 </div>
-              </Link>
-            )
-          })
-          }
+              </div>
+            </Link>
+          ))}
         </article>
       </div>
 
-
-      <div className="flex flex-col items-center">
-
-
-        <div className="inline-flex py-10 xs:mt-0">
-          <button className="flex items-center justify-center px-4 py-3 text-md font-medium text-white bg-violet-700 rounded-s hover:bg-violet-900 dark:bg-violet-800 dark:border-violet-700 dark:text-violet-400 dark:hover:bg-violet-700 dark:hover:text-white focus:bg-violet-900">
+      <div className="flex flex-col items-center pt-12 md:pt-0 mb-12">
+        <Link href='/projects'>
+          <button className="px-4 py-3 text-md font-medium text-white bg-violet-700 rounded hover:bg-violet-900 focus:bg-violet-900">
             See all projects
           </button>
-         
-        </div>
+        </Link>
       </div>
-
-
-    </div>
+    </div >
   );
 };
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,16 +5,16 @@ import React, { useState } from 'react'
 const Navbar = () => {
     const [expanded, setExpanded] = useState(false)
     return (
-        <header className="relative flex items-center top-2 justify-evenly w-full m-auto" x-data={"expanded:false"}>
+        <header className="navigation z-50 relative flex items-center top-2 justify-between w-full px-16 pt-4 pb-8" x-data={"expanded:false"}>
             {/* LOGO */}
             <div className="flex-shrink-0  ">
                 <Link href="/" title="" className="flex">
-                    <h1 className='text-2xl text-gray-300 font-black'>My <span className='text-cyan-600'>Logo</span></h1>
+                    <h1 className='text-2xl text-gray-300 font-black'>Ram <span className='text-cyan-600'>Rijal</span></h1>
                 </Link>
             </div>
 
             {/* NAVBAR */}
-            <div className=" xl:w-fit backdrop-blur-3xl rounded-sm shadow-2xl px-4 border-white max-w-7xl  ">
+            <div className=" xl:w-fit backdrop-blur-3xl bg-[#4C1D95] rounded-lg shadow-2xl px-4 border-white max-w-7xl  ">
                 <nav className="flex  items-center justify-between h-auto top-8 lg:h-16 ">
                     <div className="flex lg:hidden">
                         <button type='button' className=' p-2 ' onClick={() => setExpanded(!expanded)}>
@@ -33,7 +33,7 @@ const Navbar = () => {
                     </div>
 
                     <div className="hidden lg:flex lg:items-center lg:ml-auto lg:space-x-10">
-                        <Link href="/home" title="" className="text-xl font-normal text-gray-100 transition-all duration-200 hover:text-violet-500 focus:text-violet-500 hover:-mt-1"> Home </Link>
+                        <Link href="/" title="" className="text-xl font-normal text-gray-100 transition-all duration-200 hover:text-violet-500 focus:text-violet-500 hover:-mt-1"> Home </Link>
 
                         <Link href="/profile" title="" className="text-xl font-normal text-gray-100 transition-all duration-200 hover:text-violet-500 focus:text-violet-500 hover:-mt-1"> Profile </Link>
 
@@ -46,24 +46,24 @@ const Navbar = () => {
                 <nav className={`lg:hidden bg-violet-500 ${expanded ? 'block' : 'hidden'}`}>
                     <div className="flow-root">
                         <div className="flex flex-col px-6 -my-2 space-y-1">
-                            <Link href="#" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Home </Link>
+                            <Link href="/" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Home </Link>
 
-                            <Link href="#" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Profile </Link>
+                            <Link href="/profile" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Profile </Link>
 
-                            <Link href="#" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Projects </Link>
+                            <Link href="/projects" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Projects </Link>
 
-                            <Link href="#" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Contact </Link>
+                            <Link href="/contact" title="" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Contact </Link>
                         </div>
                     </div>
 
                     <div className="px-6 mt-6">
-                        <Link href="#" title="" className="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700" role="button">Download CV</Link>
+                        <Link href="/" title="" className="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700" role="button">Download CV</Link>
                     </div>
                 </nav>
             </div>
             {/* RESUME CV  BUTTON */}
             <div className={`lg:flex hidden`}>
-                <Link href="#" title="" className="items-center justify-center hidden px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-gradient-to-bl from-cyan-600 to-violet-800 shadow-xl border-white rounded-md lg:inline-flex hover:bg-violet-900 hover:scale-105 focus:scale-100   focus:bg-violet-700" role="button">Download CV </Link>
+                <Link href="/" title="" className="items-center justify-center hidden px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-gradient-to-bl from-cyan-600 to-violet-800 shadow-xl border-white rounded-md lg:inline-flex hover:bg-violet-900 hover:scale-105 focus:scale-100   focus:bg-violet-700" role="button">Download CV </Link>
             </div>
         </header>
 
